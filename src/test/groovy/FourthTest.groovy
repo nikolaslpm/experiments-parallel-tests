1b5a496dd0a595ee3f77891b973b85b55a158a52
Nikolas La Porta <nikolas.manne@gmail.com>
2019-11-29 23:31:34 -0300

    first commit

---
+++ src/test/groovy/FourthTest.groovy
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test
+
+class FourthTest {
+
+    @Test
+    void testOne(){
+        sleep(10000)
+        assert 1 + 1 == 2
+    }
+
+    @Test
+    void testTwo(){
+        sleep(10000)
+        assert 2 + 2 == 4
+    }
+
+    @Test
+    void testThree(){
+        sleep(10000)
+        assert 3 + 3 == 6
+    }
+
+    @Test
+    void testFour(){
+        sleep(10000)
+        assert 4 + 4 == 8
+    }
+
+    @Test
+    void testFive(){
+        sleep(10000)
+        assert 5 + 5 == 10
+    }
+}
